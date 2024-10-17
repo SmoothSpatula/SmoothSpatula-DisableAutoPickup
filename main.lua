4e44dc8c33bc78b3659a4e125a365f313a538ae4
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-10-17 21:55:57 +0200

    Added ImGui

--- main.lua
+++ main.lua
@@ -3,6 +3,14 @@
 
 mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto()
 
+local params = {
+    pickup_key = 80
+}
+
+mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.tomlfuncs then Toml = v end end 
+    params = Toml.config_update(_ENV["!guid"], params)
+end)
+
 local players_pickup = {}
 local self_pickup = false
 
@@ -11,12 +19,22 @@ function update_pickup_net(m_id, value)
 end
 
 function init()
+
+	gui.add_to_menu_bar(function()
+        local isChanged, keybind_value = ImGui.Hotkey("Pickup Keybind", params['pickup_key'])
+        if isChanged then
+            params['pickup_key'] = keybind_value
+            Toml.save_cfg(_ENV["!guid"], params)
+        end
+    end)
+
     gm.pre_script_hook(gm.constants.__lf_pPickup_step_collide_item, function(self, other, result, args)
         if (not players_pickup[args[2].value.m_id]) and args[1].value.tier ~= Item.TIER.equipment then return false end
     end)
 
     gui.add_always_draw_imgui(function()
-        if ImGui.IsKeyDown(80) then --P
+        if not gm.variable_global_get("__run_exists") then return end
+        if ImGui.IsKeyDown(params['pickup_key']) then
             if self_pickup == false then
                 self_pickup = true
                 local player = Player.get_client()
@@ -36,7 +54,3 @@ function init()
 end
 
 Initialize(init)
-
-
-if do_init then init() end
-do_init = true
